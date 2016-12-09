53f128060b881fc97b75e61ffd56b347a2913352
jrvaghela72 <jrvaghela72@gmail.com>
2016-12-09 12:57:03 +0530

    first commit

---
+++ JsonReadWrite/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace JsonReadWrite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(187, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(197, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Click to add new record";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(187, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 23);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Click to Read record";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listView1.Location = new System.Drawing.Point(12, 112);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(557, 206);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 330);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ JsonReadWrite/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace JsonReadWrite
+{
+    public partial class Form1 : Form
+    {
+        List<StudentProp> list = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list = new List<StudentProp>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StudentProp data = new StudentProp();
+            //customiste this value with you textbox value
+            data.Rollno = 1;
+            data.Name = "mike";
+            data.Cls = "MCA";
+            data.Sem = "5th";
+            data.Subjects = new SubProp()
+            {
+
+                //customiste this value with you textbox value
+                Adbms = 50,
+                An = 50,
+                Mc = 50,
+                Ns = 50,
+                Se = 50,
+                Total = 250,
+                Percentage = 50
+            };
+            list.Add(data);
+            File.WriteAllText(Path.Combine(Application.StartupPath, "Student.json"), JsonConvert.SerializeObject(list));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            using (StreamReader reader = new StreamReader(Path.Combine(Application.StartupPath, "Student.json")))
+            {
+                string json = reader.ReadToEnd();
+                List<StudentProp> readObject = JsonConvert.DeserializeObject<List<StudentProp>>(json);
+                foreach (StudentProp pr in readObject)
+                {
+                    //show item as per your requirement
+                    listView1.Items.Add(pr.Name);
+                }
+            }
+        }
+    }
+}
